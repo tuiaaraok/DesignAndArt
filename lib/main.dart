71d2767953ab01968e9fd18a7937f9036ea2313a
Timur Gruzkov <gr.timurik2001@gmail.com>
2024-11-25 13:24:11 +0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -65,6 +65,7 @@ Future<String> _initializeRemoteConfig() async {
         link = remoteConfig.getString("link");
       } catch (e) {
         log("Failed to fetch remote config: $e");
+        log("up");
       }
       if (!link.contains("showAgreebutton")) {
         box.put('link', link);
